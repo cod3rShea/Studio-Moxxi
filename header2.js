a55aa8163808750f2b9fa56382bbe2d4a95df80f
Sean Michael Shea <seanshea1337@gmail.com>
2022-02-28 08:53:27 -0500

    sync

---
+++ header2.js
@@ -0,0 +1,104 @@
+
+
+let menu = `			<nav>
+    <menu>
+      <menuitem id="demo1">
+        <a>menu</a>
+        <menu>
+          <menuitem><a href=https://discord.com/invite/f35cccbWU8?utm_source=Discord%20Widget&utm_medium=Connect TARGET="BLANK">discord</a></menuitem>
+               <menuitem>
+                  <a>profile pictures</a>
+                  <menu>
+                     <menuitem><a href=https://studiomoxxi.com/pictures_animation.html>custom animations</a></menuitem>
+                     <menuitem><a href=https://studiomoxxi.com/pictures_abstract.html>abstract animations</a></menuitem>
+                     <menuitem><a href=https://studiomoxxi.com/pictures_gif.html>gif edits</a></menuitem>
+                     <menuitem><a href=https://studiomoxxi.com/pictures_crew.html>crew pictures</a></menuitem>
+                  </menu>
+          </menuitem>
+          <menuitem>
+          <a>illusionist items</a>
+            <menu>
+               <menuitem>
+                  <a>illusionist sets</a>
+                  <menu>
+                     <menuitem><a href=https://studiomoxxi.com/illusionist_themed.html>themed sets</a></menuitem>
+                     <menuitem><a href=https://studiomoxxi.com/illustionist_wave1sets.html>original sets</a></menuitem>
+                  </menu>
+          </menuitem>
+               <menuitem>
+                  <a>illusionist slots</a>
+                  <menu>
+                     <menuitem><a href=https://studiomoxxi.com/illusionist_body.html>body</a></menuitem>
+                     <menuitem><a href=https://studiomoxxi.com/illusionist_core.html>core</a></menuitem>
+                     <menuitem><a href=https://studiomoxxi.com/illusionist_head.html>head</a></menuitem>
+                     <menuitem><a href=https://studiomoxxi.com/illusionist_neck.html>neck</a></menuitem>
+                     <menuitem><a href=https://studiomoxxi.com/illusionist_shield.html>shield</a></menuitem>
+                     <menuitem><a href=https://studiomoxxi.com/illusionist_weapon.html>weapon</a></menuitem>
+                     <menuitem><a href=https://studiomoxxi.com/illusionist_foot.html>foot</a></menuitem>
+                     <menuitem><a href=https://studiomoxxi.com/illusionist_pants.html>pants</a></menuitem>
+                     <menuitem><a href=https://studiomoxxi.com/illusionist_belt.html>belt</a></menuitem>
+                     <menuitem><a href=https://studiomoxxi.com/illusionist_ring.html>ring</a></menuitem>
+                  </menu>
+          </menuitem>
+               <menuitem>
+                  <a>illusionist reborn</a>
+                  <menu>
+                     <menuitem><a href=https://studiomoxxi.com/reborn_lifeforcegods.html>life force gods</a></menuitem>
+                     <menuitem><a href=https://studiomoxxi.com/illusionist_reborn_set.html>full set remakes</a></menuitem>
+                     <menuitem><a href=https://studiomoxxi.com/reborn_fivemangods.html>five man gods</a></menuitem>
+                     <menuitem><a href=https://studiomoxxi.com/reborn_misc.html>miscellaneous</a></menuitem>
+                  </menu>
+          </menuitem>
+          </menu>
+          </menuitem>
+              <menuitem>
+                <a>outwar themes</a>
+                        <menu>
+           <menuitem>
+                  <a>dark themes</a>
+                  <menu>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme02frame.html>red carbon</a></menuitem>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme03frame.html>lost twilight</a></menuitem>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme11frame.html>blackhand eternal</a></menuitem>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme07frame.html>blisscord</a></menuitem>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme04frame.html>ultraviolet</a></menuitem>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme16frame.html>diamond city</a></menuitem>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme06frame.html>lucid state of mind</a></menuitem>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme23frame.html>adamantium</a></menuitem>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme14frame.html>monochromatic</a></menuitem>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme13frame.html>thanox's chaos</a></menuitem>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme20frame.html>neon hyrule</a></menuitem>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme22frame.html>wakanda forever</a></menuitem>
+                  </menu>
+          </menuitem>
+                  <menuitem>
+                  <a>mid themes</a>
+                  <menu>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme17frame.html>yawn</a></menuitem>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme08frame.html>general killstein</a></menuitem>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme15frame.html>master chief</a></menuitem>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme18frame.html>fire blast</a></menuitem>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme19frame.html>shadow ball</a></menuitem>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme24frame.html>kame house</a></menuitem>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme10frame.html>demonic veil</a></menuitem>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme21frame.html>mccloud's arwing</a></menuitem>
+                  </menu>
+          </menuitem>
+                  <menuitem>
+                  <a>light themes</a>
+                  <menu>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme01frame.html>moxxi's theme</a></menuitem>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme12frame.html>vault of madness</a></menuitem>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme09frame.html>pop zyrak</a></menuitem>
+                  <menuitem><a href=https://studiomoxxi.com/ow_themes/theme05frame.html>get tropical</a></menuitem>
+                  </menu>
+          </menuitem>
+        </menu>
+              </menuitem>
+          <menuitem><a href=https://studiomoxxi.com/index.html>home</a></menuitem>
+        </menu>
+      </menuitem>
+  </nav>
+`;
+
+$("#menu").html(menu);
